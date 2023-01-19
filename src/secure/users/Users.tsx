1951a47eb43614cdae569b9d8af5a0870886f90b
isotop786 <marufislam77@gmail.com>
2023-01-20 02:22:26 +0600

    user loader added

--- src/secure/users/Users.tsx
+++ src/secure/users/Users.tsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 const Users: React.FC = () => {
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
-  const [lastPage,setLastPage] = useState(1)
+  const [lastPage,setLastPage] = useState(0)
     useEffect(() => {
         const fetchUsers = () => {
             axios.get(`users?page=${page}`)
@@ -26,43 +26,47 @@ const Users: React.FC = () => {
         
     },[page])
     return (
-        <Wrapper>
+      <Wrapper>
+      
             <h2>All Users</h2>
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap pt-3 pb-2 mb-3">
                 <div className="btn-toolbar mb-2 mb-md-0">
                     <Link to="/users/create" className="btn btn-sm btn-outline-secondary">Add</Link>
                 </div>
-        </div>     
-      <div className="table-responsive">
-        <table className="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Role</th>
-              <th>Actions</th>
+        </div>
+        {users.length == 0 ? (<div className="my-2"><h4>User's data loading...</h4></div>) : (
+     <div>
+          <div className="table-responsive">
+            <table className="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th>id</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Actions</th>
             
-            </tr>
-          </thead>
-        <tbody>
-        {users?.map((user: User) => {
-            return( <tr key={user.id}>
-            <td>{user.id}</td>
-            <td>{user.first_name}</td>
-            <td>{user.last_name}</td>
-            <td>{user.email}</td>
-            <td>{user.role?.name}</td>
-            <td><button className="btn btn-sm btn-warning">Update</button></td>
-            <td><button className="btn btn-sm btn-danger">Delete</button></td>
-            </tr>)     
-         })}               
+                </tr>
+              </thead>
+              <tbody>
+              
+                {users?.map((user: User) => {
+                  return (<tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.first_name}</td>
+                    <td>{user.last_name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.role?.name}</td>
+                    <td><button className="btn btn-sm btn-warning">Update</button></td>
+                    <td><button className="btn btn-sm btn-danger">Delete</button></td>
+                  </tr>)
+                })}
            
             
-          </tbody>
-        </table>
-        </div>
+              </tbody>
+            </table>
+          </div>
         {lastPage > 0 && (
           <nav >
           <ul className="pagination">
@@ -75,6 +79,10 @@ const Users: React.FC = () => {
           </ul>
         </nav>
       )}
+
+          </div>
+        )}
+    
         
 
         </Wrapper>
